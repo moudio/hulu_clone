b5e3cc0942c41de73e8b1013ca94daacc0386431
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-09-03 09:35:02 +0000

    Style the app header and add background color to app

--- src/Header.js
+++ src/Header.js
@@ -11,7 +11,28 @@ import huluLogo from './hulu-logo.png';
 function Header() {
   return (
     <div className="header">
-      <h1>Header</h1>
+      <div className="header__icons">
+        <div className="header__icon header__icon--active">
+          <HomeIcon />
+          <p>Home</p>
+        </div>
+        <div className="header__icon">
+          <FlashOnIcon />
+          <p>Verified</p>
+        </div>
+        <div className="header__icon">
+          <VideoLibraryIcon />
+          <p>Collections</p>
+        </div>
+        <div className="header__icon">
+          <SearchIcon />
+          <p>Search</p>
+        </div>
+        <div className="header__icon">
+          <PersonOutlineIcon />
+          <p>Account</p>
+        </div>
+      </div>
       <div className="header__icons">{/* All icons */}</div>
       <img src={huluLogo} alt="hulu logo" />
     </div>
