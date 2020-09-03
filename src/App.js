5b3b4a53c18eb58ca4fa9a39ed1f9f4787b9e16a
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-09-03 09:37:49 +0000

    Create the navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './Header';
 import './App.css';
+import Nav from './Nav';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header />
-      {/* Nav */}
+      <Nav />
     </div>
   );
 }
