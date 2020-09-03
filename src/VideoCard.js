cbcae2fa0786c5e271d6af19329ae94e625939f1
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-09-03 11:19:57 +0000

    Diplay the movie title for each movie

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import './VideoCard.css';
+import TextTruncate from 'react-text-truncate';
 const baseURL = 'https://image.tmdb.org/t/p/original';
 
 function VideoCard({ movie }) {
-  console.log(movie);
   return (
     <div className="videoCard">
       <img
         src={`${baseURL}${movie.backdrop_path || movie.poster_path}`}
         alt="Move poster"
       />
-      <p>{movie.overview}</p>
-      <h2>Movie Title</h2>
+      <TextTruncate
+        line={1}
+        element="p"
+        truncateText="..."
+        textTruncateChild={<a href="#">Read On...</a>}
+        text={movie.overview}
+      />
+      <h2>{movie.title || movie.original_name}</h2>
       <p>Number of likes...</p>
     </div>
   );
